3e9fc4b3d4e1d8ed529c563320f9d71248b30581
Ashok Vemulapalli <41159884+ashk406@users.noreply.github.com>
2024-05-03 09:44:57 +0530

    Update payment-detail-form.component.ts

--- src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import { PaymentDetailService } from '../../shared/payment-detail.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-payment-detail-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './payment-detail-form.component.html',
-  styles: ``
+  styles: ``,
 })
 export class PaymentDetailFormComponent {
+  constructor(
+    public service: PaymentDetailService,
+    private toastr: ToastrService
+  ) {}
 
+  onSubmit(form: NgForm) {
+    this.service.formSubmitted = true;
+    if (form.valid) {
+      if(this.service.formData.paymentDetailID == 0) {
+        this.insertRecord(form);
+      } else {
+        this.updateRecord(form);
+      }
+    }
+  }
+
+  insertRecord(form: NgForm) {
+    this.service.postPaymentDetail().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.service.refreshList();
+        this.service.resetForm(form);
+        this.toastr.success(
+          'Inserted Successfully!',
+          'Payment Detail Register'
+        );
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  updateRecord(form: NgForm) {
+    this.service.putPaymentDetail().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.service.refreshList();
+        this.service.resetForm(form);
+        this.toastr.info(
+          'Updated Successfully!',
+          'Payment Detail Updaed'
+        );
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
